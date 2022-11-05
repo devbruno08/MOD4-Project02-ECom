1b30e8c6ea3df564108b614df0a9b881d7e132c3
Your NameDevbruno08 <bruno_emonteiro@hotmail.com>
2022-11-05 01:37:26 -0300

    feat: active delete function

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,20 +1,38 @@
+import { MdUpdate } from "react-icons/md";
+import { api } from "../../utils/api/api";
 import { Product } from "../../utils/types/products.type";
 import { CardSection } from "./styles";
 
 interface CardProps extends Product {
-    update: () => void;
+  update: () => void;
 }
 
-export function Card({ id, name, description, price, category, imageURL, update}: CardProps){
-    return (
-        <CardSection>
-            <img src={imageURL} alt={name}/>
-            <h2>{name}</h2>
-            <span>{price}</span>
-            <p>{description}</p>
-            <button>Update</button>
-            <button>Delete</button>
-            <button>Add to cart!</button>
-        </CardSection>
-    )
+export function Card({
+  id,
+  name,
+  description,
+  price,
+  category,
+  imageURL,
+  update,
+}: CardProps) {
+
+    async function DeleteCard() {
+        const toDeleted = await api.deleteProduct(id)
+        if(toDeleted) {
+        update();
+        };
+    };
+
+  return (
+    <CardSection>
+      <img src={imageURL} alt={name} />
+      <h2>{name}</h2>
+      <span>{price}</span>
+      <p>{description}</p>
+      <button>Update</button>
+      <button onClick={DeleteCard}>Delete</button>
+      <button>Add to cart!</button>
+    </CardSection>
+  );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,38 +1,42 @@
-import { SiCockroachlabs } from "react-icons/si"
-import { MdOutlineNoteAdd } from "react-icons/md"
-import { Link, useNavigate } from "react-router-dom"
-import Modal from "react-modal"
+import { SiCockroachlabs } from "react-icons/si";
+import { MdOutlineNoteAdd } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 import { useState } from "react";
-import { HeaderComponent, HeaderLogo, HeaderSearch, HeaderSection } from "./styles"
-import { Form } from "../form/form"
+import {
+  HeaderComponent,
+  HeaderLogo,
+  HeaderSearch,
+  HeaderSection,
+} from "./styles";
+import { Form } from "../form/form";
 
 const customStyles = {
   content: {
-    top: '30%',
-    left: '70%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-80%',
-    width: '30rem',
-    height: '20rem',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '#000',
-    borderRadius: '15px',
-    border: 'none',
+    top: "30%",
+    left: "70%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-80%",
+    width: "30rem",
+    height: "20rem",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#000",
+    borderRadius: "15px",
+    border: "none",
   },
   overlay: {
-    background: 'rgba(0, 0, 0, 0.6)',
+    background: "rgba(0, 0, 0, 0.6)",
   },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 interface HeaderProps {
   controlPage: () => void;
 }
 
-export  function Header({controlPage}: HeaderProps){
-
+export function Header({ controlPage }: HeaderProps) {
   const [modalIsOpen, setModalOpen] = useState(false);
 
   function handleModal() {
@@ -41,43 +45,56 @@ export  function Header({controlPage}: HeaderProps){
 
   const navigate = useNavigate();
 
-  return (  
+  return (
     <>
-        <HeaderComponent>
-            <HeaderLogo>
-              <Link to="/">
-                <SiCockroachlabs size={23} />
-                <h1>Cheap Store</h1>   
-              </Link>             
-            </HeaderLogo>
-            <HeaderSearch>
-                <input type="text" placeholder="Search Product" />
-            </HeaderSearch>
-            <HeaderSection> 
-                <button onClick={() => {
-                    navigate("/clothes")
-                }}> Clothes </button>
-                <button onClick={() => {
-                    navigate("/equipments")
-                }}> Equipments </button>
-                <button onClick={() => {
-                    navigate("/off")
-                }}>#Off </button>
-                <button onClick={handleModal}>
-                  <MdOutlineNoteAdd size={20}/> 
-                </button>
-                <button> Add to cart!</button>
-            </HeaderSection>
-        </HeaderComponent>
-        <Modal
+      <HeaderComponent>
+        <HeaderLogo>
+          <Link to="/">
+            <SiCockroachlabs size={23} />
+            <h1>Cheap Store</h1>
+          </Link>
+        </HeaderLogo>
+        <HeaderSearch>
+          <input type="text" placeholder="Search Product" />
+        </HeaderSearch>
+        <HeaderSection>
+          <button
+            onClick={() => {
+              navigate("/clothes");
+            }}
+          >
+            {" "}
+            Clothes{" "}
+          </button>
+          <button
+            onClick={() => {
+              navigate("/equipments");
+            }}
+          >
+            {" "}
+            Equipments{" "}
+          </button>
+          <button
+            onClick={() => {
+              navigate("/off");
+            }}
+          >
+            #Off{" "}
+          </button>
+          <button onClick={handleModal}>
+            <MdOutlineNoteAdd size={20} />
+          </button>
+          <button> Add to cart!</button>
+        </HeaderSection>
+      </HeaderComponent>
+      <Modal
         isOpen={modalIsOpen}
         onRequestClose={handleModal}
         style={customStyles}
         contentLabel="Create Form"
-        >
-         <Form update={controlPage} handleModal={handleModal}/> 
-        </Modal> 
+      >
+        <Form update={controlPage} handleModal={handleModal} />
+      </Modal>
     </>
-    
   );
 }
